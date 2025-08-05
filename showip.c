4757104986ad0a8eb16ab73ba18dbe6d2ff06973
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-05 11:45:02 +0800

    remove redundant AI_PASSIVE

--- showip.c
+++ showip.c
@@ -25,9 +25,8 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	hints.ai_family = AF_UNSPEC;	
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
 
 	status = getaddrinfo(argv[1],
 				 NULL,
