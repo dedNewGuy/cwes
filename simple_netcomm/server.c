476b03a3e4f598e71ee29c2b9d01a43b4438636a
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-05 15:25:07 +0800

    add cleanup label

--- simple_netcomm/server.c
+++ simple_netcomm/server.c
@@ -10,6 +10,7 @@
 #define SERV_PORT "6969"
 
 char recv_buffer[4096];
+int err_status = 0;
 
 int main(void)
 {
@@ -28,49 +29,59 @@ int main(void)
                        &serv_info);
 	if (ai_status != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ai_status));
-		return 1;
+		err_status = 1;
 	}
 
 	int sockfd = socket(serv_info->ai_family, serv_info->ai_socktype, serv_info->ai_protocol);
 	if (sockfd < 0) {
 		perror("server socket error: ");
-		return 2;
+		err_status = 2;
+		goto cleanup;
 	}
 
 	int bind_status = bind(sockfd, serv_info->ai_addr, serv_info->ai_addrlen);
 	if (bind_status < 0) {
 		perror("server bind error: ");
-		close(sockfd);
-		return 3;
+		err_status = 3;
+		goto cleanup;
 	}
 
 	int listen_status = listen(sockfd, CONNECTION_QUEUE_LIMIT);
 	if (listen_status < 0) {
 		perror("server listen error: ");
-		close(sockfd);
-		return 4;
+		err_status = 4;
+		goto cleanup;
 	}
 
 	client_addr_len = sizeof(client_addr);
 	int client_sock = accept(sockfd, &client_addr, &client_addr_len);
 	if (client_sock < 0) {
 		perror("server accept error: ");
-		close(sockfd);
-		return 5;
+		err_status = 5;
+		goto cleanup;
 	}
 
 	ssize_t buf_size = recv(client_sock, recv_buffer, sizeof(recv_buffer), 0);
 	if (buf_size == 0) {
 		fprintf(stderr, "Client has closed the connection\n");
-		close(sockfd);
+		err_status = 6;
+		goto cleanup;
 	} else if (buf_size < 0) {
 		perror("server received error: ");
-		close(sockfd);
-		return 6;
+		err_status = 7;
+		goto cleanup;
 	}
 
 	printf("Message from client: %s\n", recv_buffer);
 
+	freeaddrinfo(serv_info);
 	close(sockfd);
 	return 0;
+
+cleanup:
+	freeaddrinfo(serv_info);
+	if (sockfd != -1) {
+		close(sockfd);
+	}
+	return err_status;
 }
