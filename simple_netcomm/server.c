5549f04dfc36a6c0c89a1402d8842fc8b4d04402
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-05 15:28:18 +0800

    organize a bit, thought still ugly

--- simple_netcomm/server.c
+++ simple_netcomm/server.c
@@ -10,7 +10,7 @@
 #define SERV_PORT "6969"
 
 char recv_buffer[4096];
-int err_status = 0;
+int exit_code = 0;
 
 int main(void)
 {
@@ -29,27 +29,34 @@ int main(void)
                        &serv_info);
 	if (ai_status != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ai_status));
-		err_status = 1;
+		exit_code = 1;
+		goto cleanup;
+	}
+
+	if (serv_info == NULL) {
+		fprintf(stderr, "No address information\n");
+		exit_code = 1;
+		goto cleanup;
 	}
 
 	int sockfd = socket(serv_info->ai_family, serv_info->ai_socktype, serv_info->ai_protocol);
 	if (sockfd < 0) {
 		perror("server socket error: ");
-		err_status = 2;
+		exit_code = 2;
 		goto cleanup;
 	}
 
 	int bind_status = bind(sockfd, serv_info->ai_addr, serv_info->ai_addrlen);
 	if (bind_status < 0) {
 		perror("server bind error: ");
-		err_status = 3;
+		exit_code = 3;
 		goto cleanup;
 	}
 
 	int listen_status = listen(sockfd, CONNECTION_QUEUE_LIMIT);
 	if (listen_status < 0) {
 		perror("server listen error: ");
-		err_status = 4;
+		exit_code = 4;
 		goto cleanup;
 	}
 
@@ -57,31 +64,28 @@ int main(void)
 	int client_sock = accept(sockfd, &client_addr, &client_addr_len);
 	if (client_sock < 0) {
 		perror("server accept error: ");
-		err_status = 5;
+		exit_code = 5;
 		goto cleanup;
 	}
 
 	ssize_t buf_size = recv(client_sock, recv_buffer, sizeof(recv_buffer), 0);
 	if (buf_size == 0) {
 		fprintf(stderr, "Client has closed the connection\n");
-		err_status = 6;
-		goto cleanup;
+		exit_code = 6;
 	} else if (buf_size < 0) {
 		perror("server received error: ");
-		err_status = 7;
-		goto cleanup;
+		exit_code = 7;
 	}
 
 	printf("Message from client: %s\n", recv_buffer);
-
-	freeaddrinfo(serv_info);
-	close(sockfd);
-	return 0;
 
 cleanup:
 	freeaddrinfo(serv_info);
 	if (sockfd != -1) {
 		close(sockfd);
 	}
-	return err_status;
+	if (client_sock != -1) {
+		close(client_sock);
+	}
+	return exit_code;
 }
